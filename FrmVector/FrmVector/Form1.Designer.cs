06d257724a220239b121f0babbbc7bbff0ff827e
Rolando8-03 <remena@uamv.edu.ni>
2025-11-04 11:49:24 -0600

    se agregan los otros ejercicios

---
+++ FrmVector/FrmVector/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace FrmVector
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.bloqueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.arreglosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vectorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.matrizToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bloqueToolStripMenuItem,
+            this.arreglosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // bloqueToolStripMenuItem
+            // 
+            this.bloqueToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salieToolStripMenuItem});
+            this.bloqueToolStripMenuItem.Name = "bloqueToolStripMenuItem";
+            this.bloqueToolStripMenuItem.Size = new System.Drawing.Size(70, 24);
+            this.bloqueToolStripMenuItem.Text = "Bloque";
+            // 
+            // salieToolStripMenuItem
+            // 
+            this.salieToolStripMenuItem.Name = "salieToolStripMenuItem";
+            this.salieToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.salieToolStripMenuItem.Text = "Salir";
+            this.salieToolStripMenuItem.Click += new System.EventHandler(this.salieToolStripMenuItem_Click);
+            // 
+            // arreglosToolStripMenuItem
+            // 
+            this.arreglosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vectorToolStripMenuItem,
+            this.matrizToolStripMenuItem});
+            this.arreglosToolStripMenuItem.Name = "arreglosToolStripMenuItem";
+            this.arreglosToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.arreglosToolStripMenuItem.Text = "Arreglos";
+            // 
+            // vectorToolStripMenuItem
+            // 
+            this.vectorToolStripMenuItem.Name = "vectorToolStripMenuItem";
+            this.vectorToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.vectorToolStripMenuItem.Text = "Vector";
+            this.vectorToolStripMenuItem.Click += new System.EventHandler(this.vectorToolStripMenuItem_Click);
+            // 
+            // matrizToolStripMenuItem
+            // 
+            this.matrizToolStripMenuItem.Name = "matrizToolStripMenuItem";
+            this.matrizToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.matrizToolStripMenuItem.Text = "Matriz";
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmPrincipal";
+            this.Text = "Menú Principal";
+            this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem bloqueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem arreglosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vectorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem matrizToolStripMenuItem;
+    }
+}
+
---
+++ FrmVector/FrmVector/FrmVector.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmVector
+{
+    public partial class FrmVector : Form
+    {
+        public static int[] vector = new int[10];
+        public static int pos =0;
+        int suma = 0;
+        double promedio = 0;
+        public FrmVector()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbNumeros_KeyDown(object sender, KeyEventArgs e)
+        {
+           if (e.KeyCode == Keys.Enter)
+           {
+                try
+                {
+                    if (int.TryParse(lblNumeros.Text, out int num))
+                    {
+                        vector[pos++] = num;
+                        for (int i = 0; i < pos; i++)
+                        {
+                            suma += vector[i];
+                            for (int j = i + 1; j < pos; j++)
+                            {
+
+                            }
+                        }
+                        promedio = suma / pos;
+                        lblPromedio.Text = promedio.ToString();
+                        
+                    }
+                }
+                catch
+                {
+
+                }
+           }
+        }
+    }
+}
